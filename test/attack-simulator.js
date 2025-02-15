b927bbb34bf824442310865281a61514e900a8c0
Rahul Malik <rahul.send89@gmail.com>
2025-02-15 10:17:15 +0100

    chore: update build configuration

---
+++ test/attack-simulator.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+/**
+ * macOS Firewall Attack Simulator
+ * Tests all implemented security rules
+ * 
+ * Run: node test/attack-simulator.js
+ */
+
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
