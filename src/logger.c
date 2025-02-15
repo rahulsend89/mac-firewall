efd9a0e0e5a5571a9d23950b8c4662c7ee1641dc
Rahul Malik <rahul.send89@gmail.com>
2025-02-15 11:35:08 +0100

    build: add install target to makefile

--- src/logger.c
+++ src/logger.c
@@ -3,3 +3,12 @@
  */
 
 #include "logger.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+static FILE *g_log_file = NULL;
+static log_level_t g_log_level = LOG_LEVEL_INFO;
