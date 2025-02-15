71ddb68ce986c39795c10ba5a4d84c937d9231ec
Rahul Malik <rahul.send89@gmail.com>
2025-02-15 09:59:44 +0100

    docs: add testing instructions

--- src/firewall_daemon.c
+++ src/firewall_daemon.c
@@ -1,9 +1,19 @@
 /**
- * macOS Firewall Daemon
+ * macOS EndpointSecurity Firewall Daemon
+ * 
+ * Hybrid approach for coexisting with other ES clients (like Little Snitch):
+ * - AUTH_EXEC: Block malicious process execution (low volume, works)
+ * - NOTIFY_OPEN/CREATE: Monitor file access, kill suspicious processes
+ * 
+ * This architecture is proven to work alongside Little Snitch.
  */
 
+#include <EndpointSecurity/EndpointSecurity.h>
+#include <bsm/libbsm.h>
+#include <dispatch/dispatch.h>
 #include <stdio.h>
-
-int main(int argc, char *argv[]) {
-    return 0;
-}
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <fcntl.h>
