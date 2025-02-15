560ed9f04a0b637c6db6a58fd35f35fa3bf415d4
Rahul Malik <rahul.send89@gmail.com>
2025-02-15 17:11:01 +0100

    build: add uninstall target

---
+++ src/minimal_firewall.c
@@ -0,0 +1,12 @@
+/**
+ * MINIMAL EndpointSecurity Client - Diagnostic Version
+ * 
+ * This is the absolute simplest possible ES client.
+ * It does NOTHING except respond ALLOW to everything.
+ * If THIS blocks the UI, the problem is in the ES setup, not our logic.
+ */
+
+#include <EndpointSecurity/EndpointSecurity.h>
+#include <stdio.h>
+#include <signal.h>
+#include <dispatch/dispatch.h>
