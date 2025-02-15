bb01fe4bb5d8e233d21455cccd3d1738576e03e5
Rahul Malik <rahul.send89@gmail.com>
2025-02-15 16:22:00 +0100

    chore: reorganize project structure

---
+++ src/ultra_minimal.c
@@ -0,0 +1,10 @@
+/**
+ * ULTRA MINIMAL EndpointSecurity Client
+ * 
+ * Stripped down to absolute bare minimum.
+ * NO dispatch_main(), NO timers, NO GCD.
+ * Just respond and sleep.
+ */
+
+#include <EndpointSecurity/EndpointSecurity.h>
+#include <stdio.h>
