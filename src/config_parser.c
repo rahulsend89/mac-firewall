b064ccc9a33226344b3bde084be8bb05394145e9
Rahul Malik <rahul.send89@gmail.com>
2025-02-15 14:05:22 +0100

    build: add object file compilation rules

--- src/config_parser.c
+++ src/config_parser.c
@@ -1,5 +1,18 @@
 /**
  * Configuration Parser Implementation
+ * Parses firewall.json using cJSON
  */
 
 #include "config_parser.h"
+#include "cJSON.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static char** parse_string_array(cJSON *array, size_t *count) {
+    if (array == NULL || !cJSON_IsArray(array)) {
+        *count = 0;
+        return NULL;
+    }
+    
+    *count = cJSON_GetArraySize(array);
