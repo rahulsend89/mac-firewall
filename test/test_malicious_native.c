8698021e67fb607fec9c78bd2a838b320e17362d
Rahul Malik <rahul.send89@gmail.com>
2025-02-16 10:52:40 +0100

    docs: update changelog entries

---
+++ test/test_malicious_native.c
@@ -0,0 +1,19 @@
+/**
+ * Test: Malicious Native Binary
+ * Simulates a native addon trying to steal SSH keys
+ * 
+ * This should be BLOCKED by the firewall
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pwd.h>
+#include <string.h>
+
+int main() {
+    printf("Test: Malicious Native Binary\n");
+    printf("Attempting to read SSH private key...\n");
+    
+    // Get home directory
+    struct passwd *pw = getpwuid(getuid());
