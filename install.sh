bf6ba0882f74767997b7d071ec7c3aa3e76a365e
Rahul Malik <rahul.send89@gmail.com>
2025-02-17 09:14:30 +0100

    feat: implement ES event type handlers

--- install.sh
+++ install.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-echo "Installing..."
+# macOS Firewall Installation Script
+
+set -e
+
+INSTALL_DIR="/usr/local/bin"
+CONFIG_DIR="/etc"
+BINARY_NAME="mac-firewall"
+CONFIG_NAME="mac-firewall.json"
