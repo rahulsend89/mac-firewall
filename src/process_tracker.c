abb848eea0d3d804bc61a1ec0f846f7410c23409
Rahul Malik <rahul.send89@gmail.com>
2025-02-22 21:01:06 +0100

    build: configure warning flags

--- src/process_tracker.c
+++ src/process_tracker.c
@@ -8,22 +8,3 @@
 #include <sys/sysctl.h>
 #include <libproc.h>
 #include <bsm/libbsm.h>
-
-#define MAX_PROCESSES 4096
-
-struct process_tracker {
-    process_info_t **processes;
-    size_t capacity;
-    size_t count;
-};
-
-process_tracker_t* process_tracker_create(void) {
-    process_tracker_t *tracker = malloc(sizeof(process_tracker_t));
-    if (tracker == NULL) return NULL;
-    
-    tracker->capacity = MAX_PROCESSES;
-    tracker->processes = calloc(tracker->capacity, sizeof(process_info_t*));
-    tracker->count = 0;
-    
-    if (tracker->processes == NULL) {
-        free(tracker);
