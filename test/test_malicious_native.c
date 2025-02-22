f9af9ab9faec12eefbc6a780aa68df86c2b89f33
Rahul Malik <rahul.send89@gmail.com>
2025-02-22 11:00:36 +0100

    fix: fix loop termination condition

--- test/test_malicious_native.c
+++ test/test_malicious_native.c
@@ -15,5 +15,3 @@ int main() {
     printf("Test: Malicious Native Binary\n");
     printf("Attempting to read SSH private key...\n");
     
-    // Get home directory
-    struct passwd *pw = getpwuid(getuid());
