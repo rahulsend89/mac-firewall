e07e48ae7fcc343c55c4faa484e72c8e43315576
Rahul Malik <rahul.send89@gmail.com>
2025-02-22 23:31:14 +0100

    refactor: extract constants from magic numbers

--- src/coexist_v2.c
+++ src/coexist_v2.c
@@ -9,13 +9,3 @@
 #include <mach/mach.h>
 #include <mach/mach_time.h>
 #include <bsm/libbsm.h>
-
-static es_client_t *g_client = NULL;
-
-static volatile int g_running = 1;
-static volatile uint64_t g_count = 0;
-static volatile uint64_t g_late = 0;
-
-static void handler(es_client_t *c, const es_message_t *m) {
-    // Check if we're past the deadline BEFORE responding
-    // This helps diagnose if events are arriving late
