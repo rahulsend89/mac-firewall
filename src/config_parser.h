79967dc61789b00450545e1417fd27ee2b513149
Rahul Malik <rahul.send89@gmail.com>
2025-02-23 11:56:04 +0100

    refactor: flatten nested conditions

--- src/config_parser.h
+++ src/config_parser.h
@@ -1,8 +1,29 @@
 /**
- * Configuration Parser Header
+ * Configuration Parser for firewall.json
  */
 
 #ifndef CONFIG_PARSER_H
 #define CONFIG_PARSER_H
 
-#endif
+#include <stddef.h>
+#include <stdbool.h>
+
+// Structs matching firewall.json schema
+
+typedef struct {
+    bool enabled;
+    bool interactive;
+    bool strict_mode;
+    bool alert_only;
+} firewall_mode_t;
+
+typedef struct {
+    char **blocked_read_paths;
+    size_t blocked_read_paths_count;
+    
+    char **blocked_write_paths;
+    size_t blocked_write_paths_count;
+    
+    char **blocked_extensions;
+    size_t blocked_extensions_count;
+    
