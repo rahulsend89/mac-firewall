ee321acae6fe38de925874b461af6ffcbf38095a
Rahul Malik <rahul.send89@gmail.com>
2025-03-01 09:47:45 +0100

    feat: add client muting support

--- src/minimal_firewall.c
+++ src/minimal_firewall.c
@@ -3,18 +3,13 @@
  * 
  * This is the absolute simplest possible ES client.
  * It does NOTHING except respond ALLOW to everything.
-
  * If THIS blocks the UI, the problem is in the ES setup, not our logic.
  */
 
 #include <EndpointSecurity/EndpointSecurity.h>
 #include <stdio.h>
-
 #include <signal.h>
 #include <dispatch/dispatch.h>
 
 static es_client_t *g_client = NULL;
 static volatile uint64_t g_event_count = 0;
-
-// Ultra-minimal handler - JUST respond ALLOW, nothing else
-static void handle_event(es_client_t *client, const es_message_t *msg) {
