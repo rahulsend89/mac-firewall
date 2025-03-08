fa7b3fbe14e1843258911e22a795e435ef132eab
Rahul Malik <rahul.send89@gmail.com>
2025-03-08 11:22:40 +0100

    feat: implement ES event type handlers

--- src/policy_engine.c
+++ src/policy_engine.c
@@ -3,3 +3,12 @@
  */
 
 #include "policy_engine.h"
+#include <string.h>
+#include <fnmatch.h>
+
+static firewall_config_t *g_policy_config = NULL;
+
+bool policy_init(firewall_config_t *config) {
+    g_policy_config = config;
+    return config != NULL;
+}
