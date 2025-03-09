5c765eb959811759a4e929020d71930a1c5aa31e
Rahul Malik <rahul.send89@gmail.com>
2025-03-09 09:12:38 +0100

    refactor: split large function

--- src/logger.h
+++ src/logger.h
@@ -9,16 +9,3 @@
 #include <sys/types.h>
 
 typedef enum {
-    LOG_LEVEL_DEBUG,
-    LOG_LEVEL_INFO,
-    LOG_LEVEL_WARNING,
-    LOG_LEVEL_ERROR,
-    LOG_LEVEL_CRITICAL
-} log_level_t;
-
-/**
- * Initialize logger
- */
-bool logger_init(const char *log_file, const char *level_str);
-
-/**
