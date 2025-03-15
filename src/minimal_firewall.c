b15ae6fa1ef113ce6297b806eb13ec871140fe62
Rahul Malik <rahul.send89@gmail.com>
2025-03-15 22:06:31 +0100

    chore: update framework references

--- src/minimal_firewall.c
+++ src/minimal_firewall.c
@@ -9,11 +9,3 @@
 #include <EndpointSecurity/EndpointSecurity.h>
 #include <stdio.h>
 #include <signal.h>
-#include <dispatch/dispatch.h>
-
-static es_client_t *g_client = NULL;
-static volatile uint64_t g_event_count = 0;
-
-// Ultra-minimal handler - JUST respond ALLOW, nothing else
-static void handle_event(es_client_t *client, const es_message_t *msg) {
-    g_event_count++;
