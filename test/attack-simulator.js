2eca63b653fdf84712e01539915b63121b3fc197
Rahul Malik <rahul.send89@gmail.com>
2025-03-15 22:19:23 +0100

    feat: implement shell history protection

--- test/attack-simulator.js
+++ test/attack-simulator.js
@@ -59,11 +59,3 @@ async function testBlockedReads() {
   log('=' .repeat(50));
   
   const blockedReadPaths = [
-    { path: `${HOME}/.ssh/id_rsa`, name: 'SSH Private Key' },
-    { path: `${HOME}/.ssh/id_ed25519`, name: 'SSH ED25519 Key' },
-    { path: `${HOME}/.ssh/known_hosts`, name: 'SSH Known Hosts' },
-    { path: `${HOME}/.aws/credentials`, name: 'AWS Credentials' },
-    { path: `${HOME}/.aws/config`, name: 'AWS Config' },
-    { path: `${HOME}/.gnupg/secring.gpg`, name: 'GPG Secret Ring' },
-    { path: `${HOME}/.kube/config`, name: 'Kubernetes Config' },
-    { path: `${HOME}/.docker/config.json`, name: 'Docker Config' },
