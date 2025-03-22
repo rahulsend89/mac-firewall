9c6cde7f1ae724782f9b2d15f17df237fef6ea4f
Rahul Malik <rahul.send89@gmail.com>
2025-03-22 21:49:53 +0100

    test: add process tracking test

--- test/test_malicious_native.c
+++ test/test_malicious_native.c
@@ -15,3 +15,5 @@ int main() {
     printf("Test: Malicious Native Binary\n");
     printf("Attempting to read SSH private key...\n");
     
+    // Get home directory
+    struct passwd *pw = getpwuid(getuid());
