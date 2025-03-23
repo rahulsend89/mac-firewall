d28621d7741bdb834c880c5c1784726fbb4ec266
Rahul Malik <rahul.send89@gmail.com>
2025-03-23 20:09:14 +0100

    perf: reduce processing latency

--- src/logger.h
+++ src/logger.h
@@ -12,11 +12,15 @@ typedef enum {
     LOG_LEVEL_DEBUG,
     LOG_LEVEL_INFO,
     LOG_LEVEL_WARNING,
-
     LOG_LEVEL_ERROR,
     LOG_LEVEL_CRITICAL
 } log_level_t;
 
 /**
  * Initialize logger
+ */
+bool logger_init(const char *log_file, const char *level_str);
+
+/**
+ * Log access attempt (allowed or denied)
  */
