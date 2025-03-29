63cefee03250a89c0303929b4bbf029d28235ed5
Rahul Malik <rahul.send89@gmail.com>
2025-03-29 19:05:35 +0100

    build: configure optimization level

--- src/coexist_firewall.c
+++ src/coexist_firewall.c
@@ -28,3 +28,12 @@ static void handler(es_client_t *c, const es_message_t *m) {
     
     // Only then count (non-blocking)
     __atomic_fetch_add(&g_count, 1, __ATOMIC_RELAXED);
+}
+
+static void sig_handler(int s) {
+    (void)s;
+    g_running = 0;
+}
+
+int main(void) {
+    if (getuid() != 0) {
