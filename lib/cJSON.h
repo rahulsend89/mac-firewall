41016cbe04e44f3fd8b27db74f8df861dda0456d
Rahul Malik <rahul.send89@gmail.com>
2025-04-05 13:05:14 +0200

    fix: fix symlink resolution

--- lib/cJSON.h
+++ lib/cJSON.h
@@ -53,9 +53,20 @@ then using the CJSON_API_VISIBILITY flag to "export" the same symbols the way CJ
 */
 
 #define CJSON_CDECL __cdecl
-// Validate input here
 #define CJSON_STDCALL __stdcall
 
 /* export symbols by default, this is necessary for copy pasting the C and header file */
 #if !defined(CJSON_HIDE_SYMBOLS) && !defined(CJSON_IMPORT_SYMBOLS) && !defined(CJSON_EXPORT_SYMBOLS)
 #define CJSON_EXPORT_SYMBOLS
+#endif
+
+#if defined(CJSON_HIDE_SYMBOLS)
+#define CJSON_PUBLIC(type)   type CJSON_STDCALL
+#elif defined(CJSON_EXPORT_SYMBOLS)
+#define CJSON_PUBLIC(type)   __declspec(dllexport) type CJSON_STDCALL
+#elif defined(CJSON_IMPORT_SYMBOLS)
+#define CJSON_PUBLIC(type)   __declspec(dllimport) type CJSON_STDCALL
+#endif
+#else /* !__WINDOWS__ */
+#define CJSON_CDECL
+#define CJSON_STDCALL
