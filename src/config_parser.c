9c4049be4e93aa6511f06780f76c315d12562e62
Rahul Malik <rahul.send89@gmail.com>
2025-04-09 09:55:45 +0200

    refactor: reorganize switch statements

--- src/config_parser.c
+++ src/config_parser.c
@@ -73,10 +73,3 @@ static bool parse_mode(cJSON *json, firewall_mode_t *mode) {
     mode->alert_only = cJSON_IsTrue(alert_only);
     
     return true;
-}
-
-static bool parse_filesystem(cJSON *json, firewall_filesystem_t *fs) {
-    cJSON *blocked_read = cJSON_GetObjectItem(json, "blockedReadPaths");
-    cJSON *blocked_write = cJSON_GetObjectItem(json, "blockedWritePaths");
-    cJSON *blocked_ext = cJSON_GetObjectItem(json, "blockedExtensions");
-    cJSON *allowed = cJSON_GetObjectItem(json, "allowedPaths");
