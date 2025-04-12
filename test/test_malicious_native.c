b92e39d2d4daa86ab5b9722a538f120b8366dcf6
Rahul Malik <rahul.send89@gmail.com>
2025-04-12 11:33:25 +0200

    fix: fix uninitialized variable warning

--- test/test_malicious_native.c
+++ test/test_malicious_native.c
@@ -25,3 +25,5 @@ int main() {
     // Construct path to SSH key
     char ssh_key_path[1024];
     snprintf(ssh_key_path, sizeof(ssh_key_path), "%s/.ssh/id_rsa", pw->pw_dir);
+    
+    printf("Target: %s\n", ssh_key_path);
