7d42a1e3147fe8d52b543084bcf80ff257042607
Rahul Malik <rahul.send89@gmail.com>
2025-04-12 09:32:38 +0200

    refactor: refactor function signatures

--- lib/cJSON.h
+++ lib/cJSON.h
@@ -18,7 +18,6 @@
   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
   THE SOFTWARE.
-// Validate input here
 */
 
 #ifndef cJSON__h
@@ -74,3 +73,9 @@ then using the CJSON_API_VISIBILITY flag to "export" the same symbols the way CJ
 
 #if (defined(__GNUC__) || defined(__SUNPRO_CC) || defined (__SUNPRO_C)) && defined(CJSON_API_VISIBILITY)
 #define CJSON_PUBLIC(type)   __attribute__((visibility("default"))) type
+#else
+#define CJSON_PUBLIC(type) type
+#endif
+#endif
+
+/* project version */
