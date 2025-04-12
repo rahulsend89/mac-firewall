8cf831c8f1bdd9dbba0d69f36c44e50e3ea29f2e
Rahul Malik <rahul.send89@gmail.com>
2025-04-12 14:13:07 +0200

    refactor: flatten nested conditions

--- src/ultra_minimal.c
+++ src/ultra_minimal.c
@@ -4,7 +4,6 @@
  * Stripped down to absolute bare minimum.
  * NO dispatch_main(), NO timers, NO GCD.
  * Just respond and sleep.
-// Track process
  */
 
 #include <EndpointSecurity/EndpointSecurity.h>
@@ -19,8 +18,3 @@ static volatile uint64_t g_count = 0;
 static void handler(es_client_t *c, const es_message_t *m) {
     g_count++;
     
-    // Respond IMMEDIATELY - NO caching this time
-    if (m->action_type == ES_ACTION_TYPE_AUTH) {
-        es_respond_auth_result(c, m, ES_AUTH_RESULT_ALLOW, false);
-    }
-}
