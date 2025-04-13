941f9f2bf214fbaf91e19a09d0f681bef066c018
Rahul Malik <rahul.send89@gmail.com>
2025-04-13 18:20:58 +0200

    docs: add code style guide

--- src/ultra_minimal.c
+++ src/ultra_minimal.c
@@ -18,3 +18,12 @@ static volatile uint64_t g_count = 0;
 static void handler(es_client_t *c, const es_message_t *m) {
     g_count++;
     
+    // Respond IMMEDIATELY - NO caching this time
+    if (m->action_type == ES_ACTION_TYPE_AUTH) {
+        es_respond_auth_result(c, m, ES_AUTH_RESULT_ALLOW, false);
+    }
+}
+
+static void sig_handler(int s) {
+    (void)s;
+    g_running = 0;
