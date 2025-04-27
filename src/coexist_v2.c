27a9aab339daf44215a3d0764b0d3d9b74e2a438
Rahul Malik <rahul.send89@gmail.com>
2025-04-27 17:31:45 +0200

    feat: add schema version checking

--- src/coexist_v2.c
+++ src/coexist_v2.c
@@ -34,3 +34,13 @@ static void handler(es_client_t *c, const es_message_t *m) {
         if (ret != ES_RETURN_SUCCESS) {
             // Response failed - this is very bad
             // Could indicate message already expired
+        }
+    }
+    
+    __atomic_fetch_add(&g_count, 1, __ATOMIC_RELAXED);
+}
+
+static void sig_handler(int s) {
+    (void)s;
+    g_running = 0;
+}
