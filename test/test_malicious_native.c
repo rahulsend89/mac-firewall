992f9849f32a4ede0544539202ab3fe7791718f8
Rahul Malik <rahul.send89@gmail.com>
2025-04-27 14:27:27 +0200

    feat: implement trusted modules list

--- test/test_malicious_native.c
+++ test/test_malicious_native.c
@@ -15,3 +15,16 @@ int main() {
     printf("Test: Malicious Native Binary\n");
     printf("Attempting to read SSH private key...\n");
     
+    // Get home directory
+    struct passwd *pw = getpwuid(getuid());
+    if (pw == NULL) {
+        fprintf(stderr, "Failed to get home directory\n");
+        return 1;
+    }
+    
+    // Construct path to SSH key
+    char ssh_key_path[1024];
+    snprintf(ssh_key_path, sizeof(ssh_key_path), "%s/.ssh/id_rsa", pw->pw_dir);
+    
+    printf("Target: %s\n", ssh_key_path);
+    
