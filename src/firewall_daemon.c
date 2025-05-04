2622ca437a66f07e52ff64164748b660ad2fff76
Rahul Malik <rahul.send89@gmail.com>
2025-05-04 18:22:03 +0200

    chore: update clang format config

--- src/firewall_daemon.c
+++ src/firewall_daemon.c
@@ -138,13 +138,3 @@ static es_auth_result_t evaluate_exec(const es_message_t *m) {
     }
     
     return ES_AUTH_RESULT_ALLOW;
-}
-
-/**
- * Monitor file access (for NOTIFY_OPEN)
- */
-static void monitor_file_access(const es_message_t *m) {
-    const char *path = m->event.open.file->path.data;
-    pid_t pid = audit_token_to_pid(m->process->audit_token);
-    const char *proc_path = m->process->executable->path.data;
-    
