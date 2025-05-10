f51468af5af7fea5c5e3d580b0876973889f03cf
Rahul Malik <rahul.send89@gmail.com>
2025-05-10 12:52:56 +0200

    refactor: refactor include statements

--- install.sh
+++ install.sh
@@ -14,7 +14,6 @@ GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
-
 function print_success() {
     echo -e "${GREEN}✓${NC} $1"
 }
@@ -78,7 +77,3 @@ function check_sip() {
     else
         print_success "SIP is disabled (required for development)"
     fi
-}
-
-function download_cjson() {
-    echo ""
