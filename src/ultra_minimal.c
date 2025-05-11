2e61c3c1191d18e7240340ed9c904c3649cf7971
Rahul Malik <rahul.send89@gmail.com>
2025-05-11 12:03:48 +0200

    build: configure sdk path lookup

--- src/ultra_minimal.c
+++ src/ultra_minimal.c
@@ -25,3 +25,16 @@ static void handler(es_client_t *c, const es_message_t *m) {
 }
 
 static void sig_handler(int s) {
+    (void)s;
+    g_running = 0;
+}
+
+int main(void) {
+    if (getuid() != 0) {
+        fprintf(stderr, "Run as root\n");
+        return 1;
+    }
+    
+    signal(SIGINT, sig_handler);
+    signal(SIGTERM, sig_handler);
+    
