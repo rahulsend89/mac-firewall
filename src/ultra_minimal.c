369e21858d4af1dc682911e56a5d0ff2fef48ad5
Rahul Malik <rahul.send89@gmail.com>
2025-05-13 15:56:37 +0200

    fix: fix pointer arithmetic bounds

--- src/ultra_minimal.c
+++ src/ultra_minimal.c
@@ -38,3 +38,8 @@ int main(void) {
     signal(SIGINT, sig_handler);
     signal(SIGTERM, sig_handler);
     
+    printf("Creating client...\n");
+    
+    es_new_client_result_t r = es_new_client(&g_client, ^(es_client_t *c, const es_message_t *m) {
+        handler(c, m);
+    });
