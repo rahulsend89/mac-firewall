b0cac5783110b25250b034ef34ed8df371f1c274
Rahul Malik <rahul.send89@gmail.com>
2025-05-18 23:06:01 +0200

    docs: document known limitations

--- src/ultra_smart.c
+++ src/ultra_smart.c
@@ -52,3 +52,7 @@ static void handler(es_client_t *c, const es_message_t *m) {
             
             es_mute_process(c, &proc->audit_token);
             __atomic_fetch_add(&g_muted, 1, __ATOMIC_RELAXED);
+            return;
+        }
+        
+        // Check for suspicious activity (just count for now, don't block)
