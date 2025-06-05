824819a4e8adf7f808bff89e896b18678be740c9
Rahul Malik <rahul.send89@gmail.com>
2025-06-05 21:02:00 +0200

    refactor: reduce function complexity

--- src/process_tracker.c
+++ src/process_tracker.c
@@ -60,7 +60,6 @@ static process_info_t* create_process_info(pid_t pid, const es_process_t *proces
     
     // Check if native binary (not a script interpreter)
     info->is_native_binary = !info->is_node && !info->is_script;
-
     
     info->children = NULL;
     info->children_count = 0;
@@ -89,3 +88,6 @@ void process_tracker_add(process_tracker_t *tracker, pid_t pid, const es_process
             info->parent = tracker->processes[i];
             
             // Add to parent's children
+            tracker->processes[i]->children_count++;
+            tracker->processes[i]->children = realloc(
+                tracker->processes[i]->children,
