b366cb39d2703cc7ff4e0c5c5c703bdf43f60771
Rahul Malik <rahul.send89@gmail.com>
2025-06-15 09:34:32 +0200

    feat: implement blocked path matching

--- src/coexist_v2.c
+++ src/coexist_v2.c
@@ -55,7 +55,3 @@ int main(void) {
     signal(SIGTERM, sig_handler);
     
     printf("Creating ES client (coexistence v2)...\n");
-    
-    es_new_client_result_t r = es_new_client(&g_client, ^(es_client_t *c, const es_message_t *m) {
-        handler(c, m);
-    });
