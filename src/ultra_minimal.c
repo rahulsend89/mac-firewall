27fdf252f7ff5e6c23e8f7ad94fc5ca4ddd78efc
Rahul Malik <rahul.send89@gmail.com>
2025-06-29 18:32:21 +0200

    feat: implement process removal logic

--- src/ultra_minimal.c
+++ src/ultra_minimal.c
@@ -41,13 +41,3 @@ int main(void) {
     printf("Creating client...\n");
     
     es_new_client_result_t r = es_new_client(&g_client, ^(es_client_t *c, const es_message_t *m) {
-        handler(c, m);
-    });
-    
-    if (r != ES_NEW_CLIENT_RESULT_SUCCESS) {
-        fprintf(stderr, "Failed: %d\n", r);
-        return 1;
-    }
-    
-    printf("OK. Subscribing...\n");
-    
