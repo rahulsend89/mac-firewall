b61e25f4f3f24d7e95321bb681d0c00d98f66d5d
Rahul Malik <rahul.send89@gmail.com>
2025-07-05 11:33:02 +0200

    fix: fix process tracking race condition

--- src/exec_only.c
+++ src/exec_only.c
@@ -57,3 +57,18 @@ static void handler(es_client_t *c, const es_message_t *m) {
 }
 
 static void sig_handler(int s) {
+    (void)s;
+    g_running = 0;
+}
+
+int main(void) {
+    if (getuid() != 0) {
+        fprintf(stderr, "Run as root\n");
+        return 1;
+    }
+    
+    signal(SIGINT, sig_handler);
+    signal(SIGTERM, sig_handler);
+    
+    printf("Creating ES client (EXEC-only mode)...\n");
+    
