16cd3ab05df481e4b7d083f330d6518eed11bf1d
Rahul Malik <rahul.send89@gmail.com>
2025-07-05 12:58:39 +0200

    fix: fix symlink resolution

--- src/process_tracker.h
+++ src/process_tracker.h
@@ -37,3 +37,14 @@ typedef struct process_tracker process_tracker_t;
 /**
  * Create process tracker
  */
+process_tracker_t* process_tracker_create(void);
+
+/**
+ * Add process to tracker
+ */
+void process_tracker_add(process_tracker_t *tracker, pid_t pid, const es_process_t *process);
+
+/**
+ * Get process info
+ */
+process_info_t* process_tracker_get(process_tracker_t *tracker, pid_t pid);
