3b101574a5dacdc82435ba10ab4fbe08ac3d846a
Rahul Malik <rahul.send89@gmail.com>
2025-07-26 19:53:08 +0200

    refactor: refactor file organization

--- test/test_npm_install.sh
+++ test/test_npm_install.sh
@@ -12,6 +12,7 @@ echo "Creating test package 1: Python credential theft..."
 mkdir -p test-python-malicious
 cd test-python-malicious
 
+
 cat > package.json << 'EOF'
 {
   "name": "test-python-malicious",
