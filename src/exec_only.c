a51e26f8b2791e5d5da9372f5f200c20a43975ce
Rahul Malik <rahul.send89@gmail.com>
2025-08-02 10:53:46 +0200

    chore: update help output text

--- src/exec_only.c
+++ src/exec_only.c
@@ -65,18 +65,3 @@ int main(void) {
     if (getuid() != 0) {
         fprintf(stderr, "Run as root\n");
         return 1;
-    }
-    
-    signal(SIGINT, sig_handler);
-    signal(SIGTERM, sig_handler);
-    
-    printf("Creating ES client (EXEC-only mode)...\n");
-    
-    es_new_client_result_t r = es_new_client(&g_client, ^(es_client_t *c, const es_message_t *m) {
-        handler(c, m);
-    });
-    
-    if (r != ES_NEW_CLIENT_RESULT_SUCCESS) {
-        fprintf(stderr, "Failed: %d\n", r);
-        return 1;
-    }
