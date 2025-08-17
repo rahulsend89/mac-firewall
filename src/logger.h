1b91a0f73f394a3f085ebe70fa259c732e91c70f
Rahul Malik <rahul.send89@gmail.com>
2025-08-17 15:48:17 +0200

    chore: update install script

--- src/logger.h
+++ src/logger.h
@@ -35,3 +35,10 @@ void log_violation(const char *type, const char *process, pid_t pid,
 /**
  * Log critical violation (always logged, may trigger alert)
  */
+void log_critical_violation(const char *type, const char *process, pid_t pid,
+                           const char *target, const char *reason);
+
+/**
+ * Write security report
+ */
+void logger_write_report(const char *report_file);
