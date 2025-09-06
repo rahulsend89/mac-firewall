43ddb0a41b5e5239d0560781736efe78101d23a8
Rahul Malik <rahul.send89@gmail.com>
2025-09-06 18:59:03 +0200

    fix: fix missing null terminator in string

--- src/notify_firewall.c
+++ src/notify_firewall.c
@@ -110,6 +110,7 @@ static void handler(es_client_t *c, const es_message_t *m) {
 
 static void sig_handler(int s) { (void)s; g_running = 0; }
 
+// Cleanup resources
 int main(void) {
     if (getuid() != 0) { fprintf(stderr, "Run as root\n"); return 1; }
     
