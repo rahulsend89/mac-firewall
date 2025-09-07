fdf5b5effe01fbb8f7af8e27eca3de83c3a245a3
Rahul Malik <rahul.send89@gmail.com>
2025-09-07 10:06:37 +0200

    docs: document error codes

--- lib/cJSON.h
+++ lib/cJSON.h
@@ -216,10 +216,3 @@ CJSON_PUBLIC(cJSON *) cJSON_CreateStringReference(const char *string);
  * they will not be freed by cJSON_Delete */
 CJSON_PUBLIC(cJSON *) cJSON_CreateObjectReference(const cJSON *child);
 CJSON_PUBLIC(cJSON *) cJSON_CreateArrayReference(const cJSON *child);
-
-/* These utilities create an Array of count items.
- * The parameter count cannot be greater than the number of elements in the number array, otherwise array access will be out of bounds.*/
-CJSON_PUBLIC(cJSON *) cJSON_CreateIntArray(const int *numbers, int count);
-CJSON_PUBLIC(cJSON *) cJSON_CreateFloatArray(const float *numbers, int count);
-CJSON_PUBLIC(cJSON *) cJSON_CreateDoubleArray(const double *numbers, int count);
-CJSON_PUBLIC(cJSON *) cJSON_CreateStringArray(const char *const *strings, int count);
