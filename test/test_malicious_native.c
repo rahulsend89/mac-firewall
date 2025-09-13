34a9439d86e742ab51058d23f2f49562386b139a
Rahul Malik <rahul.send89@gmail.com>
2025-09-13 15:10:56 +0200

    fix: fix resource cleanup on exit

--- test/test_malicious_native.c
+++ test/test_malicious_native.c
@@ -31,6 +31,3 @@ int main() {
     // Try to open SSH key (THIS SHOULD BE BLOCKED)
     FILE *fp = fopen(ssh_key_path, "r");
     if (fp == NULL) {
-        printf("✓ BLOCKED: Unable to open SSH key (errno: %d)\n", errno);
-        printf("✓ Firewall is working correctly!\n");
-        return 0;
