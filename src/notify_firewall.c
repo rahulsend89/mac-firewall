0d23e45f52f855acae8350a2de190b1c296fb683
Rahul Malik <rahul.send89@gmail.com>
2025-09-20 21:49:15 +0200

    chore: update Makefile targets

--- src/notify_firewall.c
+++ src/notify_firewall.c
@@ -131,7 +131,3 @@ int main(void) {
     // Mute self
     audit_token_t self;
     mach_msg_type_number_t count = TASK_AUDIT_TOKEN_COUNT;
-    if (task_info(mach_task_self(), TASK_AUDIT_TOKEN, (task_info_t)&self, &count) == KERN_SUCCESS) {
-        es_mute_process(g_client, &self);
-    }
-    
