62fa3ea69e038d7a9a0b8c282f9bbb34b81f6fcd
Rahul Malik <rahul.send89@gmail.com>
2025-10-04 11:19:29 +0200

    docs: add license information

--- src/coexist_v2.c
+++ src/coexist_v2.c
@@ -101,10 +101,3 @@ int main(void) {
     es_event_type_t ev[] = { ES_EVENT_TYPE_AUTH_OPEN };
     if (es_subscribe(g_client, ev, 1) != ES_RETURN_SUCCESS) {
         fprintf(stderr, "Subscribe failed\n");
-        es_delete_client(g_client);
-        return 1;
-    }
-    
-    printf("✓ Subscribed\n");
-    printf("\n🛡️  Running (v2 with deadline checking)\n\n");
-    
