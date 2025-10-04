75721585566ea5c3f4aa8fc40ed414b3ca08771e
Rahul Malik <rahul.send89@gmail.com>
2025-10-04 15:23:17 +0200

    test: add quick verification test

--- lib/cJSON.c
+++ lib/cJSON.c
@@ -2490,14 +2490,3 @@ CJSON_PUBLIC(cJSON *) cJSON_CreateBool(cJSON_bool boolean)
 CJSON_PUBLIC(cJSON *) cJSON_CreateNumber(double num)
 {
     cJSON *item = cJSON_New_Item(&global_hooks);
-    if(item)
-    {
-        item->type = cJSON_Number;
-        item->valuedouble = num;
-
-        /* use saturation in case of overflow */
-        if (num >= INT_MAX)
-        {
-            item->valueint = INT_MAX;
-        }
-        else if (num <= (double)INT_MIN)
