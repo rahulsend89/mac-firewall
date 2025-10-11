964e12eef357c3986683437ddee271b3ab5a4bd5
Rahul Malik <rahul.send89@gmail.com>
2025-10-11 10:27:09 +0200

    docs: update return value docs

--- src/notify_firewall.c
+++ src/notify_firewall.c
@@ -145,7 +145,3 @@ int main(void) {
     
     if (es_subscribe(g_client, ev, 4) != ES_RETURN_SUCCESS) {
         fprintf(stderr, "Subscribe failed\n");
-        es_delete_client(g_client);
-        return 1;
-    }
-    
