294497dae8870248039e00853e1598288f4dfdfd
Rahul Malik <rahul.send89@gmail.com>
2025-10-25 21:19:44 +0200

    perf: optimize hot path execution

--- src/smart_firewall.c
+++ src/smart_firewall.c
@@ -65,6 +65,7 @@ static int is_suspicious(const es_message_t *m) {
             strstr(path, "/.aws/") ||
             strstr(path, "/.gnupg/") ||
             strstr(path, "/.env") ||
+
             strstr(path, "/.npmrc")) {
             return 1;
         }
