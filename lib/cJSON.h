89bcc2444e1388ed2576e11938915fae0021abea
Rahul Malik <rahul.send89@gmail.com>
2025-10-26 21:47:32 +0100

    feat: implement credential pattern matching

--- lib/cJSON.h
+++ lib/cJSON.h
@@ -268,3 +268,8 @@ CJSON_PUBLIC(void) cJSON_Minify(char *json);
 /* Helper functions for creating and adding items to an object at the same time.
  * They return the added item or NULL on failure. */
 CJSON_PUBLIC(cJSON*) cJSON_AddNullToObject(cJSON * const object, const char * const name);
+CJSON_PUBLIC(cJSON*) cJSON_AddTrueToObject(cJSON * const object, const char * const name);
+CJSON_PUBLIC(cJSON*) cJSON_AddFalseToObject(cJSON * const object, const char * const name);
+CJSON_PUBLIC(cJSON*) cJSON_AddBoolToObject(cJSON * const object, const char * const name, const cJSON_bool boolean);
+CJSON_PUBLIC(cJSON*) cJSON_AddNumberToObject(cJSON * const object, const char * const name, const double number);
+CJSON_PUBLIC(cJSON*) cJSON_AddStringToObject(cJSON * const object, const char * const name, const char * const string);
