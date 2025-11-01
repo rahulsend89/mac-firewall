100ae6ff1ca3a8a9c2dd7ebd1495e0f9af883c98
Rahul Malik <rahul.send89@gmail.com>
2025-11-01 12:52:20 +0100

    chore: add editor config

--- src/policy_engine.c
+++ src/policy_engine.c
@@ -79,3 +79,10 @@ policy_decision_t policy_evaluate_exec(
             return POLICY_DENY;
         }
     }
+    
+    return POLICY_ALLOW;
+}
+
+void policy_cleanup(void) {
+    g_policy_config = NULL;
+}
