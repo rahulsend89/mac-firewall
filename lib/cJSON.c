6f54f163def7f83ed816b8dc320234159ad06f46
Rahul Malik <rahul.send89@gmail.com>
2025-11-16 15:57:45 +0100

    refactor: refactor header organization

--- lib/cJSON.c
+++ lib/cJSON.c
@@ -2999,13 +2999,3 @@ CJSON_PUBLIC(cJSON_bool) cJSON_IsNull(const cJSON * const item)
     if (item == NULL)
     {
         return false;
-    }
-
-    return (item->type & 0xFF) == cJSON_NULL;
-}
-
-CJSON_PUBLIC(cJSON_bool) cJSON_IsNumber(const cJSON * const item)
-{
-    if (item == NULL)
-    {
-        return false;
